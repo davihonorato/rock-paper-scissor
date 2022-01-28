2b4198ab7889acb435f664297117f4346ec44ec9
Davi Honorato <davihonoratoctt@gmail.com>
2022-01-28 15:39:38 -0300

    lidando com a escolha do usuário e do computador

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const gameBox = document.querySelector(".game");
 
 function getInput(buttonId) {
+    //opções disponíveis
+    let choices = ['rock', 'paper', 'scissor'];
+
     //pegando a escolha do usuário
-    let userChoice;
-    if(buttonId == 'rock') {
-        userChoice = 1;
-    }else if(buttonId == 'paper') {
-        userChoice = 2;
-    }else if(buttonId == 'scissor') {
-        userChoice = 3;
-    }
+    let userChoice = buttonId;
 
     //pegando a escolha do computador
-    let computerChoice = 2;
+    let computerChoice = choices[getComputerChoiceId(choices.length)];
 
+    //processa e exibe o resultado na tela
     showResult(userChoice, computerChoice);
+}
+
+//retorna um número gerado aleatóriamente que é utilizando como uma key para encontrar um valor em um array
+function getComputerChoiceId(qntOptions) {
+    return Math.floor(Math.random() * qntOptions);
 }
 
 function showResult(user, computer){
